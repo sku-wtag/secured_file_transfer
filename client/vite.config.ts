72e3fadb739c029145f9b68d491460114d1d6a66
Shorojit Kumar Sarkar <152980765+sku-wtag@users.noreply.github.com>
2026-08-28 16:44:05 +0600

    change front-end default port

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -29,7 +29,7 @@ export default defineConfig(({ mode }) => ({
     exclude: ['shared'],
   },
   server: {
-    port: 5173,
+    port: 5100,
     proxy: {
       '/api': {
         target: apiProxyTarget,
